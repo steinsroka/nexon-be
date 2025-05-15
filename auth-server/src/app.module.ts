ca5665cf90d4b8a06b6b95d4a1fa0e8e97a5248c
steinsroka <steinsroka@gmail.com>
2025-05-16 01:04:17 +0900

    [NX-4] fix: 주석 제거

--- auth-server/src/app.module.ts
+++ auth-server/src/app.module.ts
@@ -17,16 +17,12 @@ import { PassportModule } from '@nestjs/passport';
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => (
-        console.log(configService.get<string>('MONGODB_URI')),
-        console.log(configService.get<string>('PORT')),
-        {
-          uri: configService.get<string>(
-            'MONGODB_URI',
-            'mongodb://localhost:27017/nexon-auth',
-          ),
-        }
-      ),
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>(
+          'MONGODB_URI',
+          'mongodb://localhost:27017/nexon-auth',
+        ),
+      }),
     }),
     PassportModule,
     JwtModule.registerAsync({
