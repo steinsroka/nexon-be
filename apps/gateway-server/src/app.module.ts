ebf329902ec70ca17ee92690aaef459b9b4f3832
박해규 <62500287+steinsroka@users.noreply.github.com>
2025-05-18 00:47:13 +0900

    [NX-15] GatewayService 추가 #15

--- apps/gateway-server/src/app.module.ts
+++ apps/gateway-server/src/app.module.ts
@@ -1,21 +1,15 @@
+import { MicroServiceType } from '@lib/enums/microservice.enum';
+import { JwtStrategy } from '@lib/strategies/jwt.strategy';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { PassportModule } from '@nestjs/passport';
-import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './controllers/auth.controller';
-import { AuthService } from './services/auth.service';
-import { UserService } from './services/user.service';
-import { JwtStrategy } from '@lib/strategies/jwt.strategy';
-import { EventService } from './services/event.service';
-import { UserController } from './controllers/user.controller';
 import { EventController } from './controllers/event.controller';
-import { RewardService } from './services/reward.service';
 import { RewardController } from './controllers/reward.controller';
-
-export const AUTH_SERVICE = 'AUTH_SERVICE';
-export const USER_SERVICE = 'USER_SERVICE';
-export const EVENT_SERVICE = 'EVENT_SERVICE';
+import { UserController } from './controllers/user.controller';
+import { GatewayService } from './gateway.service';
 
 @Module({
   imports: [
@@ -26,7 +20,7 @@ export const EVENT_SERVICE = 'EVENT_SERVICE';
     ClientsModule.registerAsync([
       {
         imports: [ConfigModule],
-        name: AUTH_SERVICE,
+        name: MicroServiceType.AUTH_SERVICE,
         useFactory: () => ({
           transport: Transport.TCP,
           options: {
@@ -39,7 +33,7 @@ export const EVENT_SERVICE = 'EVENT_SERVICE';
         inject: [ConfigService],
       },
       {
-        name: EVENT_SERVICE,
+        name: MicroServiceType.EVENT_SERVICE,
         useFactory: () => ({
           transport: Transport.TCP,
           options: {
@@ -70,12 +64,6 @@ export const EVENT_SERVICE = 'EVENT_SERVICE';
     EventController,
     RewardController,
   ],
-  providers: [
-    AuthService,
-    UserService,
-    EventService,
-    RewardService,
-    JwtStrategy,
-  ],
+  providers: [JwtStrategy, GatewayService],
 })
 export class AppModule {}
---
+++ apps/gateway-server/src/gateway.service.ts
@@ -0,0 +1,66 @@
+import { MicroServiceType } from '@lib/enums/microservice.enum';
+import { Injectable, Inject } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ClientProxy } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
+import { Response } from 'express';
+
+@Injectable()
+export class GatewayService {
+  constructor(
+    @Inject('AUTH_SERVICE') private readonly authServiceClient: ClientProxy,
+    @Inject('EVENT_SERVICE') private readonly eventServiceClient: ClientProxy,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async sendRequest<RES, REQ = any>(
+    service: MicroServiceType,
+    pattern: string,
+    data: REQ,
+  ): Promise<RES> {
+    try {
+      const client = (() => {
+        switch (service) {
+          case MicroServiceType.AUTH_SERVICE:
+            return this.authServiceClient;
+          case MicroServiceType.EVENT_SERVICE:
+            return this.eventServiceClient;
+          default:
+            // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+            throw new Error(`Unknown service: ${service}`);
+        }
+      })();
+      return await firstValueFrom(client.send<RES>(pattern, data));
+    } catch (error) {
+      console.error(
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        `[GatewayService.sendRequest] service: ${service} pattern: ${pattern} data: ${data}}`,
+        error,
+      );
+      throw error;
+    }
+  }
+
+  public setRefreshTokenCookie(res: Response, token: string): void {
+    const isSecureCookie =
+      this.configService.get<string>('NODE_ENV') === 'production';
+    const domain = this.configService.get<string>('COOKIE_DOMAIN');
+
+    res.cookie('refresh_token', token, {
+      httpOnly: true,
+      secure: isSecureCookie,
+      sameSite: 'strict',
+      maxAge: 1 * 24 * 60 * 60 * 1000, // 1일 (밀리초)
+      path: '/auth/refresh',
+      ...(domain && { domain }),
+    });
+  }
+
+  public clearRefreshTokenCookie(res: Response): void {
+    res.clearCookie('refresh_token', {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+    });
+  }
+}
